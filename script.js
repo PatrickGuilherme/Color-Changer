0223db79e7c4bbe8e35c76bc050d9a957b4ab9af
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2021-07-18 01:12:59 -0300

    Adição de comentários da função de mudarCor

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//Mudar as corres
 function MudarCor(){
    let corCaixaA = BuscarCorCaixa("caixaA");
    let corCaixaB = BuscarCorCaixa("caixaB");
@@ -5,13 +7,16 @@ function MudarCor(){
    console.log(`cor caixa a: ${corCaixaA}`);
    console.log(`cor caixa b: ${corCaixaB}`);
 
+   //Trocar as cores
    let transferir =  corCaixaA;
    corCaixaA = corCaixaB;
    corCaixaB = transferir;
    
+   //Exibir RGB utilizado
    document.getElementById("corRGB-a").textContent =  corCaixaA;
    document.getElementById("corRGB-b").textContent =  corCaixaB;
    
+    //Trocar a cor das caixas
    document.getElementById("caixaA").style.backgroundColor = corCaixaA;
    document.getElementById("caixaB").style.backgroundColor = corCaixaB;
 }
